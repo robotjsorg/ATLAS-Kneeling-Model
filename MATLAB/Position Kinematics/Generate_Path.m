1cbfcd3d12b19c8942444b8ae8f253f65a96abd1
Joe McMahon <jmcmahon443@gmail.com>
2015-11-06 14:18:49 -0500

    more paths examples

--- MATLAB/Position Kinematics/Generate_Path.m
+++ MATLAB/Position Kinematics/Generate_Path.m
@@ -1,9 +1,11 @@
 function( q_matrix ) = Generate_Path( q_state_start, q_state_final )
 	// 1. Take a step
 	// Should start at start state
-	new_foot_position = Forward_Transform( q_state, "lfoot", "rfoot" );
+	lfoot_to_rfoot = Forward_Transform( q_state, "lfoot", "lfoot" );
 
 	// 2. Lower the butt
+	lfoot_to_pelvis = Forward_Transform( q_state, "lfoot", "pelvis" );
+	lfoot_from_pelvis = Inverse_Transform( q_state, "lfoot", "pelvis" );
 
 	// 3. Bend the knees
 
