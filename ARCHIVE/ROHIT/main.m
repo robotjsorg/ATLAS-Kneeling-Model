f0d3957516ce06a1e47557a027e7a5cd15ef568c
rohitsheth <rssheth@wpi.edu>
2015-11-06 23:48:33 -0500

    Getting Stuff initialized
    DH transform and making a plot function as well as reducing the size of
    transformation matrices terms. Will be done more comprehensively over
    the weekend.

---
+++ ARCHIVE/ROHIT/main.m
@@ -0,0 +1,17 @@
+% init statements
+
+% dh table
+% symbolic transforms and jacobians except values for q
+% tranforms and jacobians for right knee, right toe, left arm, right arm, pelvis
+
+% COM data for all
+% Moment of  Inertia data for all
+
+% initial and final config
+% plot whole body
+
+% update transformations with inital position values
+% generate path of q values to end position
+
+Curr_joints = [ zeros(1,20) ] ;
+T = plotatlas(Curr_joints) 
---
+++ MATLAB/Position Kinematics/dhtransform.m
@@ -0,0 +1,43 @@
+function [ Trans ] = dhtransform( params )
+% DHtrans implements the DH parameters and outputs the transformation
+% matrix created by those parameters.
+% Input - a 4 element vector corresponding to [d, theta, a, alpha]
+% Ouptut - the 4x4 homogeneous transformation matrix associated with the
+% given DH parameters. 
+% This is a copy of the other DHparams function, with the difference that it
+% contains the functions for sin and cos in radians.
+
+% Extract the DH parameters from the input vector
+d     = params(1);
+theta = params(2);
+a     = params(3);
+alpha = params(4);
+
+% Transform for aligning previous x with common normal
+Rotz = [cos(theta), -sin(theta), 0, 0;
+        sin(theta),  cos(theta), 0, 0;
+                0,            0, 1, 0;
+                0,            0, 0, 1];
+            
+% Transform for translating along z
+Transz = [1, 0, 0, 0; 
+          0, 1, 0, 0; 
+          0, 0, 1, d; 
+          0, 0, 0, 1];
+      
+% Transform for translating along x (common normal)
+Transx = [1, 0, 0, a;
+          0, 1, 0, 0;
+          0, 0, 1, 0; 
+          0, 0, 0, 1];
+      
+% Tranform for rotation about x to align previous z with new z axis
+Rotx = [1,          0,           0, 0;
+        0, cos(alpha), -sin(alpha), 0;
+        0, sin(alpha),  cos(alpha), 0;
+        0,          0,           0, 1];
+
+%Calculate the net 4x4 homogeneous transformation matrix 
+Trans = vpa(simplify(Rotz*Transz*Transx*Rotx));
+end
+
