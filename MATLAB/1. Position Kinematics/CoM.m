4d3b21037d1439284a8d79f5372d0047a3105a8f
Joe McMahon <jmcmahon443@gmail.com>
2015-11-08 17:37:50 -0500

    Organizing.

---
+++ MATLAB/1. Position Kinematics/CoM.m
@@ -0,0 +1,10 @@
+% Outputs the X, Y, Z coordinates of the CoM of a given q_state.
+function[ xyz ] = CoM( q_state ){
+	% Calculate the X, Y, Z coordinates of each of the link's individual CoM.
+
+	% Average all the links' ( mass * CoM ) and divide by the total mass of the links.
+
+	xyz = [ 0 0 0 ];
+
+	return xyz;
+}
--- MATLAB/1. Position Kinematics/Position_Kinematics.m
+++ MATLAB/1. Position Kinematics/Position_Kinematics.m
@@ -1,4 +1,4 @@
-q_state_start = [];
+% Defq_state_start = [];
 q_state_finish = [];
 q_matrix = Generate_Path( q_state_start, q_state_finish );
 display( q_matrix );
