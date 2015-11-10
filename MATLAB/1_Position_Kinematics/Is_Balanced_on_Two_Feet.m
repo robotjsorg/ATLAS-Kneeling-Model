52ab6504b6670a0049a305373aec7bb70bd66a66
Joe McMahon <jmcmahon443@gmail.com>
2015-11-10 17:51:59 -0500

    more dummy files

--- MATLAB/1_Position_Kinematics/Is_Balanced_on_Two_Feet.m
+++ MATLAB/1_Position_Kinematics/Is_Balanced_on_Two_Feet.m
@@ -1,4 +1,6 @@
-% THADDEUS IS WORKING ON THIS.
+% SRISHTI IS WORKING ON THIS.
+% width of foot ~ 0.133887m
+% height of foot ~ 0.227000m
 
 function [ is_it ] = Is_Balanced_on_Two_Feet( q_state, CoM )
 	% Check if the CoM is balanced by the given q state (which should be balanced on LFoot)
