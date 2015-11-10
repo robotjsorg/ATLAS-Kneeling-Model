2c1f74f847c49c6d3caf24c83831b490ad008ef6
Joe McMahon <jmcmahon443@gmail.com>
2015-11-10 18:09:50 -0500

    Update feet distance

--- MATLAB/1_Position_Kinematics/Is_Balanced_on_LFoot.m
+++ MATLAB/1_Position_Kinematics/Is_Balanced_on_LFoot.m
@@ -1,6 +1,7 @@
 % SRISHTI IS WORKING ON THIS.
 % width of foot ~ 0.133887m
 % height of foot ~ 0.227000m
+% distance between feet while standing = 0.023m
 
 function [ is_it ] = Is_Balanced_on_LFoot( q_state, CoM )
 	% Check if the CoM is balanced by the given q state (which should be balanced on LFoot)
