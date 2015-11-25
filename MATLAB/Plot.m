1a6fe3ce954fbd05f5264e3df80806e5a1abfb81
Joe McMahon <jmcmahon443@gmail.com>
2015-11-25 15:19:51 -0500

    dynamics update

--- MATLAB/Plot.m
+++ MATLAB/Plot.m
@@ -1,10 +1,11 @@
+addpath('R:\ATLAS-Kneeling-Model\MATLAB\Kinematics');
+addpath('R:\ATLAS-Kneeling-Model\MATLAB\3DPlot');
+
 global q;
 global LFootRFoot;
 global PelvisLArm;
 global PelvisRArm;
 
-addpath('R:\ATLAS-Kneeling-Model\MATLAB\Kinematics');
-addpath('R:\ATLAS-Kneeling-Model\MATLAB\3DPlot');
 run('q1.m');
 run('Data.m');
 
@@ -14,8 +15,8 @@ UTorsoLArm = struct;
 
 PositionKinematics( q );
 Positions();
-
-% Name the links in memory by their joints
-% {'pelvis';'ltorso';'mtorso';'utorso';'lclav';'lscap';'luarm';'llarm';'lufarm';'llfarm';'lhand';'head';'hokuyolink';'rclav';'rscap';'ruarm';'rlarm';'rufarm';'rlfarm';'rhand';'luglut';'llglut';'luleg';'llleg';'ltalus';'lfoot';'ruglut';'rlglut';'ruleg';'rlleg';'rtalus';'rfoot'};
+LinkCentersofMasses();
 
 PlotATLAS();
+PlotCenterOfMass();
+PlotMinimumFootprintPolygon();
