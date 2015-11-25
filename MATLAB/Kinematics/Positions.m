d6a70f86f6a115050b20ac35c45af35b457ed1c9
Joe McMahon <jmcmahon443@gmail.com>
2015-11-25 15:07:43 -0500

    removed redundant data, added link mass and stuff

--- MATLAB/Kinematics/Positions.m
+++ MATLAB/Kinematics/Positions.m
@@ -1,20 +1,20 @@
 function[] = Positions()
     global LFootRFoot;
-    global PelvisLArm;
     global PelvisRArm;
+    global UTorsoLArm;
 
     numberOfJoints = length(LFootRFoot);
     for i = 1:numberOfJoints
         LFootRFoot(i).position = LFootRFoot(i).base(1:3,4);
     end
 
-    numberOfJoints = length(PelvisLArm);
-    for i = 1:numberOfJoints
-        PelvisLArm(i).position = PelvisLArm(i).base(1:3,4);
-    end
-
     numberOfJoints = length(PelvisRArm);
     for i = 1:numberOfJoints
         PelvisRArm(i).position = PelvisRArm(i).base(1:3,4);
+    end
+
+    numberOfJoints = length(UTorsoLArm);
+    for i = 1:numberOfJoints
+        UTorsoLArm(i).position = UTorsoLArm(i).base(1:3,4);
     end
 end
--- MATLAB/Plot.m
+++ MATLAB/Plot.m
@@ -1,25 +1,21 @@
-addpath('R:\ATLAS-Kneeling-Model\MATLAB\Kinematics');
-addpath('R:\ATLAS-Kneeling-Model\MATLAB\3DPlot');
-
 global q;
 global LFootRFoot;
 global PelvisLArm;
 global PelvisRArm;
 
+addpath('R:\ATLAS-Kneeling-Model\MATLAB\Kinematics');
+addpath('R:\ATLAS-Kneeling-Model\MATLAB\3DPlot');
 run('q1.m');
-% run('Data.m');
+run('Data.m');
 
 LFootRFoot = struct;
-PelvisLArm = struct;
 PelvisRArm = struct;
+UTorsoLArm = struct;
 
 PositionKinematics( q );
 Positions();
 
-% LinkCentersofMasses = LinkCentersofMasses( Positions );
-% CenterofMass = CenterofMass( LinkCentersofMasses );
-% MinimumFootprintPolygon = MinimumFootprintPolygon( Positions );
+% Name the links in memory by their joints
+% {'pelvis';'ltorso';'mtorso';'utorso';'lclav';'lscap';'luarm';'llarm';'lufarm';'llfarm';'lhand';'head';'hokuyolink';'rclav';'rscap';'ruarm';'rlarm';'rufarm';'rlfarm';'rhand';'luglut';'llglut';'luleg';'llleg';'ltalus';'lfoot';'ruglut';'rlglut';'ruleg';'rlleg';'rtalus';'rfoot'};
 
 PlotATLAS();
-% PlotCenterofMass( LinkCentersofMasses );
-% PlotMinimumFootprintPolygon( MinimumFootprintPolygon );
