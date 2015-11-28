348a68530118a762666fcb2970ef597681fbba9a
Joe McMahon <jmcmahon443@gmail.com>
2015-11-28 16:46:43 -0500

    Instead of inverting the axes, move the camera!!

--- MATLAB/3DPlot/PlotATLAS.m
+++ MATLAB/3DPlot/PlotATLAS.m
@@ -8,10 +8,10 @@ function[] = PlotATLAS()
     points = updatePoints();
 
     Atlasfig = figure;
-    plot1.output = line( points(3,1:13), -points(2,1:13), points(1,1:13), 'LineWidth', 2 );
-    plot2.output = line( points(3,[6,14:16]), -points(2,[6,14:16]), points(1,[6,14:16]), 'LineWidth', 2 );
-    plot3.output = line( points(3,16:18), -points(2,16:18), points(1,16:18), 'LineWidth', 2 );
-    plot4.output = line( points(3,[16,19:20]), -points(2,[16,19:20]), points(1,[16,19:20]), 'LineWidth', 2 );
+    plot1.output = line( points(1,1:13), points(2,1:13), points(3,1:13), 'LineWidth', 2 );
+    plot2.output = line( points(1,[6,14:16]), points(2,[6,14:16]), points(3,[6,14:16]), 'LineWidth', 2 );
+    plot3.output = line( points(1,16:18), points(2,16:18), points(3,16:18), 'LineWidth', 2 );
+    plot4.output = line( points(1,[16,19:20]), points(2,[16,19:20]), points(3,[16,19:20]), 'LineWidth', 2 );
     set(plot1.output, 'Marker', 'o');
     set(plot2.output, 'Marker', 'o');
     set(plot3.output, 'Marker', 'o');
@@ -26,7 +26,7 @@ function[] = PlotATLAS()
     xlim([-1.5 1.5]); ylim([-1.5 1.5]); zlim([-1.5 1.5]);
     xlabel('X'); ylabel('Y'); zlabel('Z');
     grid on;
-    view([4,-4,2]);
+    view([4,4,4]);
     
     min = [ -25 -52 0   -30 -92 -45 -38 -45 -92 -30  0   -52 -25 -38 -30 -12 -45 -90 -45 -90 ];
     max = [  25  28 135  30  37  10  38  10  37  30  135  28  25  38  30  30  90  90  90  90 ];
@@ -63,18 +63,18 @@ function[] = updatePlot( i, text, eventName )
     points = updatePoints();
 
     set(text,'string',num2str(newq));
-    set(plot1.output,'xdata',points(3,1:13));
-    set(plot1.output,'ydata',-points(2,1:13));
-    set(plot1.output,'zdata',points(1,1:13));
-    set(plot2.output,'xdata',points(3,[6,14:16]));
-    set(plot2.output,'ydata',-points(2,[6,14:16]));
-    set(plot2.output,'zdata',points(1,[6,14:16]));
-    set(plot3.output,'xdata',points(3,16:18));
-    set(plot3.output,'ydata',-points(2,16:18));
-    set(plot3.output,'zdata',points(1,16:18));
-    set(plot4.output,'xdata',points(3,[16,19:20]));
-    set(plot4.output,'ydata',-points(2,[16,19:20]));
-    set(plot4.output,'zdata',points(1,[16,19:20]));
+    set(plot1.output,'xdata',points(1,1:13));
+    set(plot1.output,'ydata',points(2,1:13));
+    set(plot1.output,'zdata',points(3,1:13));
+    set(plot2.output,'xdata',points(1,[6,14:16]));
+    set(plot2.output,'ydata',points(2,[6,14:16]));
+    set(plot2.output,'zdata',points(3,[6,14:16]));
+    set(plot3.output,'xdata',points(1,16:18));
+    set(plot3.output,'ydata',points(2,16:18));
+    set(plot3.output,'zdata',points(3,16:18));
+    set(plot4.output,'xdata',points(1,[16,19:20]));
+    set(plot4.output,'ydata',points(2,[16,19:20]));
+    set(plot4.output,'zdata',points(3,[16,19:20]));
     drawnow;
 end
 
