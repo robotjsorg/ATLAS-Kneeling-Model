651cbe3e7919e0ede5fc497d3e738257e84fc356
Joe McMahon <jmcmahon443@gmail.com>
2015-11-28 15:40:18 -0500

    Made Plot Work Great

--- MATLAB/Kinematics/Positions.m
+++ MATLAB/Kinematics/Positions.m
@@ -5,18 +5,18 @@ function[] = Positions()
     global TorsoRArm;
     
     for i = 1:length(LFootRFoot)
-        LFootRFoot(i).position = LFootRFoot(i).base(1:3,4) - LFootRFoot(7).base(1:3,4);
+        LFootRFoot(i).position = LFootRFoot(i).base(1:3,4) - LFootRFoot(6).base(1:3,4);
     end
 
     for i = 1:length(PelvisTorso)
-        PelvisTorso(i).position = PelvisTorso(i).base(1:3,4) - LFootRFoot(7).base(1:3,4);
+        PelvisTorso(i).position = PelvisTorso(i).base(1:3,4) - LFootRFoot(6).base(1:3,4);
     end
 
     for i = 1:length(TorsoLArm)
-        TorsoLArm(i).position = TorsoLArm(i).base(1:3,4) - LFootRFoot(7).base(1:3,4);
+        TorsoLArm(i).position = TorsoLArm(i).base(1:3,4) - LFootRFoot(6).base(1:3,4);
     end
     
     for i = 1:length(TorsoRArm)
-        TorsoRArm(i).position = TorsoRArm(i).base(1:3,4) - LFootRFoot(7).base(1:3,4);
+        TorsoRArm(i).position = TorsoRArm(i).base(1:3,4) - LFootRFoot(6).base(1:3,4);
     end
 end
