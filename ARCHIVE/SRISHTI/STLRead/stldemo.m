3cdd002f3261074bcde7b73c0d6c4521dd7965c4
srishtisr <ssrivastava@wpi.edu>
2015-12-08 14:42:14 -0500

    Patch version of 3d plot

--- ARCHIVE/SRISHTI/STLRead/stldemo.m
+++ ARCHIVE/SRISHTI/STLRead/stldemo.m
@@ -17,12 +17,12 @@ close all;
 
 % Import an STL mesh, returning a PATCH-compatible face-vertex structure
 fv = stlread('r_clav.stl');
-fv1 = stlread('l_clav.stl');
-fv1.faces=fv1.faces+.3633;
-fv1.vertices=fv1.vertices-.10000;
-fv.faces=[fv.faces;fv1.faces];
-fv.vertices=[fv.vertices;fv1.vertices];
-plot3(fv.vertices(:,1),fv.vertices(:,2),fv.vertices(:,3));
+% fv1 = stlread('l_clav.stl');
+% fv1.faces=fv1.faces+.3633;
+% fv1.vertices=fv1.vertices-.10000;
+% fv.faces=[fv.faces;fv1.faces];
+% fv.vertices=[fv.vertices;fv1.vertices];
+% plot3(fv.vertices(:,1),fv.vertices(:,2),fv.vertices(:,3));
 %% Making new arrays for x y z
 % tryface=[fv.faces;fv1.faces];
 % tryver=[fv.vertices;fv1.vertices];
@@ -35,12 +35,12 @@ plot3(fv.vertices(:,1),fv.vertices(:,2),fv.vertices(:,3));
 % hold on
 % 
 
-% ax=newplot;
-% set(gcf,'Renderer','zbuffer')
-%  patch('Faces',fv.faces,'Vertices',fv.vertices,'FaceColor',       [0 0 0], ...
-%          'EdgeColor',       [1 1 1] , ...
-%          'FaceLighting',    'gouraud',     ...
-%          'AmbientStrength', 0.15);
+ax=newplot;
+set(gcf,'Renderer','zbuffer')
+ patch('Faces',fv.faces,'Vertices',fv.vertices,'FaceColor',       [1 1 1], ...
+         'EdgeColor',      [ 0 0 0] , ...
+         'FaceLighting',    'gouraud',     ...
+         'AmbientStrength', 0.15);
 %  view(ax,3) 
 %    hold on
 %  
@@ -50,10 +50,10 @@ plot3(fv.vertices(:,1),fv.vertices(:,2),fv.vertices(:,3));
 %      Create two polygons by specifying x and y as two-column matrices.
 %      Each column defines the coordinates for one of the polygons.
 
-% % Add a camera light, and tone down the specular highlighting
-% camlight('headlight');
-% material('dull');
-% 
-% % Fix the axes scaling, and set a nice view angle
-% axis('image');
-% view([-135 35]);
+% Add a camera light, and tone down the specular highlighting
+camlight('headlight');
+material('dull');
+
+% Fix the axes scaling, and set a nice view angle
+axis('image');
+view([-135 35]);
